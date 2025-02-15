e2bbe75b464b52f626321c27faf4d26aaf658f7f
anesHamdaoui <bacetd2004@gmail.com>
2025-02-15 21:25:55 +0100

    update the print function

--- GraphTheory/Trees/Tnode.cpp
+++ GraphTheory/Trees/Tnode.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-const int maxC = 4;
+const int maxC = 1;
 
 struct Tnode
 {
@@ -30,25 +30,15 @@ void fillTree(Tree &t1, char c)
     for (int i = 0; i < 5; i++)
     {
         t1[i].node = c + i;
-        // set<int> used;  // Track unique children
-        printf("%c : ", (char)(c + i));
+        printf("%c : \n", (char)(c + i));
         for (int j = 0; j < maxC; j++)
         {
-            /*
-            int randChild;
-            do {
-                randChild = rand() % 26;  // Ensure valid child index
-            } while (randChild == i || used.count(randChild));  // Avoid self-loops & duplicates
-            */
-
-            // t1[i].child[j] = randChild;
             printf("%d : ", j + 1);
-            cin >> t1[i].child[j]; // i will enter 0 to indicate nothing 
-            // used.insert(randChild);
+            cin >> t1[i].child[j];
         }
     }
 }
-
+/*
 void PrintTree(Tree t1)
 {
     cout << "strict digraph {\n";
@@ -61,44 +51,39 @@ void PrintTree(Tree t1)
             for (int j = 0; j < maxC; j++)
             {
                 int childIdx = t1[i].child[j];
-                if (childIdx != 0) // why it's print this relation node -> 0 ? i dosn't want this 
+                if (childIdx != 0) // why it's print this relation node -> 0 ? i dosn't want this
                 {
-                    string edge = string(1, t1[i].node) + " -> " + t1[childIdx].node;
-                    cout << edge << "\n"; 
+                    string edge = string(1, t1[i].node) + " -> " + (char)((int)(t1[0].node)  + (t1[childIdx].child[j])- 1 );
+                    cout << edge << "\n";
                 }
-                /*
+                
                 if (!edges.count(edge)) {  // Avoid duplicate edges
                 cout << edge << "\n";
                 edges.insert(edge);
             }
-            */
-            }
+
         }
     }
-    cout << "}\n";
+}
+cout << "}\n";
 }
 
+*/
 void PrintTree1(Tree t1)
 {
     cout << "strict digraph {\n";
 
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < 26; i++)
     {
         if (t1[i].node != '0')
         {
-            bool hasChildren = false;
             for (int j = 0; j < maxC; j++)
             {
                 int childIdx = t1[i].child[j];
                 if (childIdx >= 0 && childIdx < 26 && t1[childIdx].node != '0')
                 {
-                    cout << t1[i].node << " -> " << t1[childIdx].node << ";\n";
-                    hasChildren = true;
+                    cout << t1[i].node << " -> " << t1[t1[i].child[j] - 1].node << "\n";
                 }
-            }
-            if (!hasChildren)
-            {
-                cout << t1[i].node << ";\n"; // Ensure nodes without children are printed
             }
         }
     }
